0142e60322530e36e177748669be0a496584d6fe
Nathan Luu <nateluu28@gmail.com>
2021-06-05 11:12:10 -0700

    Added server comments

---
+++ frontend/components/server/server.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Server extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    this.props.fetchServers()
+      .then(() => this.setState({loading: false}));
+    console.log(this.state);
+  }
+
+  render() {
+    let serverNames;
+    if (!this.props.loading) {
+      let servers = Object.values(this.props.servers);
+      serverNames = servers.map((server,i) => (
+        <li>
+            {server.name}
+        </li>
+        // Create server index item in the future 
+        // make it clickable
+      ));
+    }
+
+
+    return (
+      <div>
+        <span>
+          servers!
+        </span>
+        {serverNames}
+      </div>
+
+    )
+  }
+}
+
+export default Server;
